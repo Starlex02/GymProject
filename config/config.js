f4e54554d3d28889cb7eb4f80658a412d03c844b
DChumak24 <d.chumak@crm.studio>
2024-09-10 17:26:57 +0300

    add testConfig to configuration file

--- config/config.js
+++ config/config.js
@@ -1,6 +1,29 @@
 export const config = {
-    host: "localhost",
-    user: "root",
-    password: "admin",
-    database: "gymproject",
+    db: {
+        host: process.env.DB_HOST || 'localhost',
+        user: process.env.DB_USER || 'root',
+        password: process.env.DB_PASS || 'admin',
+        database: process.env.DB_NAME || 'gymproject',
+        port: process.env.DB_PORT || '3306',
+        dialect: process.env.DB_DIALECT || 'mysql'
+    },
+    app: {
+        port: process.env.port || '3000',
+        clientHost: process.env.CLIENT_HOST || 'http://localhost:3000',
+    },
+}
+
+export const testConfig = {
+    db: {
+        host: process.env.DB_HOST || 'localhost',
+        user: process.env.DB_USER || 'root',
+        password: process.env.DB_PASS || 'admin',
+        database: process.env.DB_NAME || 'test_db',
+        port: process.env.DB_PORT || '3306',
+        dialect: process.env.DB_DIALECT || 'mysql'
+    },
+    app: {
+        port: process.env.port || '3000',
+        clientHost: process.env.CLIENT_HOST || 'http://localhost:3000',
+    },
 }
