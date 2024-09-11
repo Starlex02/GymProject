aa45b258847f95c9813f2705dcc45d29f2e1c478
DChumak24 <d.chumak@crm.studio>
2024-09-11 09:23:27 +0300

    Implement WorkoutPrograms class

---
+++ models/workout-programs.js
@@ -0,0 +1,35 @@
+import { Model } from "sequelize";
+
+class WorkoutPrograms extends Model {
+    static init(sequelize, DataTypes) {
+        return super.init({
+            id: {
+                type: DataTypes.INTEGER,
+                autoIncrement: true,
+                primaryKey: true
+            },
+            user_id: {
+                type: DataTypes.INTEGER,
+                allowNull: false,
+                references: {
+                    model: 'users',
+                    key: 'id'
+                }
+            },
+            name: {
+                type: DataTypes.STRING,
+                allowNull: false
+            },
+            description: {
+                type: DataTypes.STRING
+            },
+            image: {
+                type: DataTypes.STRING
+            }
+        }, {
+            sequelize
+        })
+    }
+}
+
+export default WorkoutPrograms;
