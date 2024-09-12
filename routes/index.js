88460a309b1cb55094a8381c7c871480c0bcb586
--global <starlexplay24@gmail.com>
2024-09-12 17:43:03 +0300

    add main router with routes for signin, signup, logout, and home

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,14 @@
 import express from "express";
+import signin from "./signin.js";
+import signup from "./signup.js";
+import logout from "./logout.js";
+import home from "./home.js";
 
 const router = express.Router();
+
+router.use("/signin", signin);
+router.use("/signup", signup);
+router.use("/logout", logout);
+router.use("/home", home);
 
 export default router;
