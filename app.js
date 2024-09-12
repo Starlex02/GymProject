6d15f969118a0d8df1fa9930c23452781e1dfb2d
--global <starlexplay24@gmail.com>
2024-09-12 17:27:34 +0300

    add body-parser, multer and session import to app.js

--- app.js
+++ app.js
@@ -1,10 +1,25 @@
 import express from "express";
 import route from "./routes/index.js";
+import bodyParser from "body-parser";
+import multer from "multer";
+import session from "express-session";
 
 const app = express();
+const upload = multer();
 
 app.set("view engine", "pug");
 app.set('views', './views');
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.use(upload.array());
+
+app.use(session({
+    secret: 'secret',
+    resave: true,
+    saveUninitialized: true
+}));
 
 app.use(express.static("public"));
 app.use(route);
