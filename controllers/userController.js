15e0308985528ba8adf31afde97a2337b3e549db
--global <starlexplay24@gmail.com>
2024-09-17 14:52:42 +0300

    Add try-catch to signup and signin func in UserController

--- controllers/userController.js
+++ controllers/userController.js
@@ -3,33 +3,50 @@ import bcrypt from "bcrypt";
 
 class UserController {
     async signup(req, res) {
-        const userExist = await User.findOne({where: {email: req.body.email}});
-
-        if(userExist) {
-            return res.status(400).json({message: "User already exist"});            
+        try {
+            const userExist = await User.findOne({ where: { email: req.body.email } });
+    
+            if (userExist) {
+                return res.status(400).json({ message: "User already exists" });
+            }
+    
+            const hashedPassword = await bcrypt.hash(req.body.password, 10);
+    
+            await User.create({
+                email: req.body.email,
+                password: hashedPassword
+            });
+    
+            req.session.user = { email: req.body.email };
+    
+            req.session.save(() => {
+                res.status(200).json();
+            });
+        } catch (error) {
+            res.status(500).json({ message: error.message });
+            console.log(error.message);
         }
-
-        const hashedPassword = await bcrypt.hash(req.body.password, 10);
-
-        User.create({
-            email: req.body.email,
-            password: hashedPassword
-        });
-
-        req.session.user = {email: req.body.email};
-        res.redirect("home");
     }
+    
 
     async signin(req, res) {
-        const userExist = await User.findOne({where: {email: req.body.email}});
+        try {
+            const userExist = await User.findOne({where: {email: req.body.email}});
 
-        if(userExist && await bcrypt.compare(req.body.password, userExist.password)) {
-            req.session.user = {email: req.body.email};
-            res.redirect("home");   
-            return;
+            if(userExist && await bcrypt.compare(req.body.password, userExist.password)) {
+                req.session.user = {email: req.body.email};
+
+                req.session.save(() => {
+                    res.status(200).json();
+                });  
+                return;
+            }
+
+            res.status(400).json({message: "User not found"});  
+        } catch (error) {
+            res.status(500).json({ message: error.message });
+            console.log(error);
         }
-
-        return res.status(400).json({message: "User not found"});  
     }
 
     logout(req, res) {
