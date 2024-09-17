a312debe3595a938bcfa6bd90e53580fcdabfd03
--global <starlexplay24@gmail.com>
2024-09-17 14:50:09 +0300

    Add form submission handler with async func

---
+++ public/js/formHandler.js
@@ -0,0 +1,33 @@
+export default async function formHandler(event) {
+    event.preventDefault();
+    
+    const form = event.target;
+
+    try {
+        const response = await submitForm(form);
+        handleResponse(response);
+    } catch (error) {
+        alert(error.message);
+        console.log(error);
+    }
+}
+
+async function submitForm (form) {
+    const response = await fetch(`${form.action}`, {
+        method: "POST",
+        body: new FormData(form),
+    });
+
+    if (!response.ok) {
+        const error = await response.json();
+        throw new Error(error.message);
+    }
+
+    return response;
+}
+
+function handleResponse(response) {
+    if (response.ok) {
+        window.location.href = "/home";
+    }
+}
