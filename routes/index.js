71d0844580490fda2984b7556ba5fa806f2cbed1
--global <starlexplay24@gmail.com>
2024-09-26 09:46:36 +0300

    add today routes to index

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,7 @@ import express from "express";
 import signin from "./signin.js";
 import signup from "./signup.js";
 import logout from "./logout.js";
+import today from "./today.js";
 import home from "./home.js";
 
 const router = express.Router();
@@ -9,6 +10,7 @@ const router = express.Router();
 router.use("/signin", signin);
 router.use("/signup", signup);
 router.use("/logout", logout);
-router.use("/home", home);
+router.use("/today", today);
+router.use("/", home);
 
 export default router;
